0e98d6e96565324ced9475638623d7e6ebea5dbb
BScheihing <bruno.scheihing@ing.uchile.cl>
2015-11-20 23:49:31 -0300

    Crea script para estimar constante de Hubble

---
+++ Hubble.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+np.random.seed(347)
+
+def fit_data(data):
+
+    return H0
+
+datosHubble = np.loadtxt("data/hubble_original.dat")
+datosSNI = np.loadtxt("data/SNIa.dat", usecols=(1,2))
